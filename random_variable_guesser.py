dbef8c3ebbe260aba38e39c1406a061f7ef6ce21
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-09-10 23:27:37 -0400

    cohesive refactor

--- random_variable_guesser.py
+++ random_variable_guesser.py
@@ -18,35 +18,37 @@ def log_expectation(key_to_log_val_func, random_var: RandomVariable): # inject f
     return reduce(softmax_combine,
                     (log2_prob + key_to_log_val_func(key) for key, log2_prob in random_var.log_probabilities.items())
                     )
-        
 
-def max_expected_log_probability_guess(clue_and_keyword_to_log_prob_func, random_vars: Sequence[RandomVariable], clues: tuple[str]) -> [float, tuple[int]]:
+# wrap return for more readable and cohesive output
+ 
+@dataclass(frozen=True)
+class Guess:
+    log_expected_probability: float = 0.0
+    code: tuple[int] = tuple()
+    
+
+def max_log_expected_probability_guess(clue_and_keyword_to_log_prob_func, random_vars: Sequence[RandomVariable], clues: tuple[str]) -> Guess:
     keyword_to_log_prob_given_clue_funcs = (partial(clue_and_keyword_to_log_prob_func, clue) for clue in clues)
     log_expected_probability_given_clue_funcs = [partial(log_expectation, keyword_to_log_prob_given_clue_func) for keyword_to_log_prob_given_clue_func in keyword_to_log_prob_given_clue_funcs]
 
     # smells like dp, could see if caching helps if this ends up being bottleneck
-    def max_expected_log_prob(var_indices=tuple(range(len(random_vars))), clue_indices=tuple(range(len(clues)))):
-        if not clue_indices:
-            return 0.0, tuple()
-        
-        clue_index, *remaining_clue_indices = clue_indices
-        remaining_clue_indices = tuple(remaining_clue_indices)
-                
-        max_expected_log_probability = -math.inf
-        best_guess = None
+    def max_log_expected_prob_guess(var_indices=tuple(range(len(random_vars))), clue_index = 0) -> Guess:
+        if clue_index == len(clues):
+            return Guess() # probability 1, empty code
+                        
+        best_guess = Guess(-math.inf) # start at probability 0, is like starting max at -inf
 
         # try each available random variable and see which yields best match probability
         for i, var_index in enumerate(var_indices):
             remaining_var_indices = var_indices[:i] + var_indices[i + 1:]
             
-            max_subproblem_expected_log_probability, guess = max_expected_log_prob(remaining_var_indices, remaining_clue_indices)
-            expected_log_probability = max_subproblem_expected_log_probability + log_expected_probability_given_clue_funcs[clue_index](random_vars[var_index])
+            subproblem_best_guess = max_log_expected_prob_guess(remaining_var_indices, clue_index + 1)
+            log_expected_probability = subproblem_best_guess.log_expected_probability + log_expected_probability_given_clue_funcs[clue_index](random_vars[var_index])
             
             # update max log probability and corresponding code guess
-            if expected_log_probability > max_expected_log_probability:
-                max_expected_log_probability = expected_log_probability
-                best_guess = (var_index,) + guess
+            if log_expected_probability > best_guess.log_expected_probability:
+                best_guess = Guess(log_expected_probability, (var_index,) + subproblem_best_guess.code)
                 
-        return max_expected_log_probability, best_guess
+        return best_guess
     
-    return max_expected_log_prob()
+    return max_log_expected_prob_guess()
