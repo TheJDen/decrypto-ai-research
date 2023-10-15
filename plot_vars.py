6d8c9e7533887ea370a4d43b595b83d2dff230ac
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2023-10-14 23:55:07 -0400

    py files for reuse

---
+++ plot_vars.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy_guesser as npg
+
+def plot_random_var(random_var: npg.NumpyRandomVariable, ax: plt.Axes):
+    data = dict(zip(random_var.keyword_indices, np.exp(random_var.log_probabilities)))
+    sns.barplot(data, ax=ax).set(ylim=(0, 1))
+
+def plot_random_vars(random_vars, axs, round=0):
+    for vi, random_var in enumerate(random_vars):
+        plot_random_var(random_var, axs[vi, round])
